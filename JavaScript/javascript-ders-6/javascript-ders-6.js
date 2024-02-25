928f577b8cf38752d36bca4bffa95b189aa83988
Mursel43 <yusiflimursel43@gmail.com>
2024-02-25 21:41:04 +0400

    Update 1.0.3

---
+++ JavaScript/javascript-ders-6/javascript-ders-6.js
@@ -0,0 +1,63 @@
+// var a=prompt("Cumle yazin.");
+// var i=0;
+// while(i<a.length){
+//     console.log(a[i]);
+//     i++
+// }
+
+// var ad =prompt("Adınızı daxil edin...");
+// while(!ad.trim()){
+//     ad=prompt("Xaiş edirəm adınızı doğru yazın!");
+// }
+// console.log(`${ad}, xoş gəlmisiz!`);
+
+// var ad =prompt("Adınızı daxil edin: ");
+// while(true){
+//     if (!ad.trim()){
+//         ad=prompt("Xaiş olunur adınızı doğru yazın!");
+//     }
+//     else{
+//         break;
+//     }
+// }
+// console.log(`${ad}, xoş gəldiniz!`);
+
+// var ad =prompt("Adınızı daxil edin: ");
+// var i=1;
+// while(i<3){
+//     if (!ad.trim()){
+//         i++
+//         ad=prompt("Xaiş olunur adınızı doğru yazın!");
+//     }
+//     else{
+//         break;
+//     }
+// }
+// if(!ad.trim()){
+//     console.log("Siz artıq 3 dəfə cəht etmisiniz. Xaiş olunur biraz sonra yenidən cəht edin!");
+// }
+// else{
+//     console.log(`${ad}, xoş gəldiniz!`);
+// }
+
+// var ad = "Murad";
+// for (let i in ad){         // bu for in dövrü ad dəyçəninin içindəki indeksləri götrür və typeof -u stringdir.
+//     console.log(i);
+// }
+
+// var soyad="Məmmədov";
+// for (let g of soyad){    // for of isə soyad dəyçəninin içndəki sinvolları götürür.
+//     console.log(g);
+// }
+
+// function repeatStr (n, s) {
+//     var cem='';
+//     var i=0;
+//     while(i<n){
+//       cem+=s;
+//       i++
+//     }
+//     return cem;
+//   }
+
+// console.log(repeatStr(12,"h"));
